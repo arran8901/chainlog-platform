05d2efc818e04ebe732b7a13927c6a8c5ce0a335
Arran O'S <arran.os.signup@gmail.com>
2022-06-02 14:47:53 +0100

    AllContracts query: implement query handler keeper method

--- x/contract/keeper/grpc_query_all_contracts.go
+++ x/contract/keeper/grpc_query_all_contracts.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/arran8901/chainlog-platform/x/contract/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -16,8 +17,29 @@ func (k Keeper) AllContracts(goCtx context.Context, req *types.QueryAllContracts
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	var contracts []types.SmartContractWithAddress
 
-	return &types.QueryAllContractsResponse{}, nil
+	contractStore := smartContractStore(k, ctx)
+
+	// Paginate contracts based on req.Pagination
+	pageRes, err := query.Paginate(contractStore, req.Pagination, func(key []byte, value []byte) error {
+		var contract types.SmartContract
+		if err := k.cdc.Unmarshal(value, &contract); err != nil {
+			return err
+		}
+		contractAddress := sdk.AccAddress(key[len(types.SmartContractKeyPrefix):])
+		contracts = append(contracts, types.SmartContractWithAddress{
+			ContractAddress: contractAddress.String(),
+			Code:            contract.Code,
+			DynamicKb:       contract.DynamicKb,
+		})
+		return nil
+	})
+
+	// Internal error if pagination failed
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryAllContractsResponse{Contracts: contracts, Pagination: pageRes}, nil
 }
