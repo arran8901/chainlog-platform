9d6b59eb9115635ca8ddff0e7fe658fe0a5a252a
Arran O'S <arran.os.signup@gmail.com>
2022-06-03 10:48:19 +0100

    CallContract ValidateBasic: parse contract address and return errors, parse and validate coin and return errors, require coin is of denomination 'token'

--- x/contract/types/message_call_contract.go
+++ x/contract/types/message_call_contract.go
@@ -44,5 +44,22 @@ func (msg *MsgCallContract) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+
+	// Parse contract address
+	// Return error if parsing failed – do not check if the address has a contract yet
+	_, err = sdk.AccAddressFromBech32(msg.ContractAddress)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid contract address: %s (%s)", msg.ContractAddress, err.Error())
+	}
+
+	// Parse and validate coin
+	value, err := sdk.ParseCoinNormalized(msg.Value)
+	if err != nil || !value.IsValid() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "invalid coin value: %s", msg.Value)
+	}
+	if value.Denom != SmartContractCoinDenom {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidCoins, "invalid coin denomination: %s, expected %s", value.Denom, SmartContractCoinDenom)
+	}
+
 	return nil
 }
